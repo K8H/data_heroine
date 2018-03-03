6711ec6b7cc1d3d02995daf5a65f288b73b8fb77
K8H <k8hofmann@gmail.com>
2018-03-03 21:01:07 +0100

    Add Sqlite Functionalities
    Functionalities ('compute_avg_weekly_price' and
    'get_week_of_max_relative_span') were added, with tests respectively. It
    executes queries on sqlite database.

--- data/sqlite/sqlite_computations.py
+++ data/sqlite/sqlite_computations.py
@@ -1,32 +1,33 @@
 import csv
 import sqlite3
 
+import pandas as pd
 from computation_provider import ComputationProvider
 
 
 class Sqlite(ComputationProvider):
 
-    def __init__(self):
+    def __init__(self, test=False):
         """
 
         """
         super().__init__()
         self.db_name = self.config_parser.get('database', 'file_name')
         self.db_table = self.config_parser.get('database', 'table_name')
-        self.download_crypto_curr_to_csv()
-        self.create_sqlite_table()
 
-    def create_sqlite_table(self, test=False):
+        self.file_name = '%s/%s' % (self.dir_path, self.time_series_file_name)
+        if test:
+            self.file_name = '%s/sqlite/test/%s' % (self.dir_path, self.time_series_file_name)
+
+        self.download_crypto_curr_to_csv()
+        self.connection = self.create_sqlite_table()
+
+    def create_sqlite_table(self):
         """
         Method establish sqlite database connection, creates data table and import data from csv file.
     
-        :param test: if True (running in test mode), name of the csv file gets 'sqlite/test/' prefix
         :return: opened connection with sqlite db
         """
-        file_name = '%s/%s' % (self.dir_path, self.time_series_file_name)
-        if test:
-            file_name = '%s/sqlite/test/%s' % (self.dir_path, self.time_series_file_name)
-
         self.print_datetime_output('Connect to data base %s' % self.db_name)
         con = sqlite3.connect(self.db_name)
         cur = con.cursor()
@@ -37,23 +38,54 @@ class Sqlite(ComputationProvider):
             self.print_datetime_output('Previous table %s was dropped' % self.db_table)
             cur.execute("DROP TABLE %s;" % self.db_table)
 
-        self.print_datetime_output('Create table %s and import data from csv file %s' % (self.db_table, self.time_series_file_name))
-        cur.execute(
-            "CREATE TABLE %s (timestamp, close_USD);" % self.db_table)
+        self.print_datetime_output('Create table %s and import data from csv file %s' % (self.db_table,
+                                                                                         self.time_series_file_name))
+        cur.execute("CREATE TABLE %s (timestamp, close_USD);" % self.db_table)
 
-        with open(file_name, 'r') as fin:
+        with open(self.file_name, 'r') as fin:
             dr = csv.DictReader(fin)
             to_db = [(i['timestamp'], i['close (USD)']) for i in dr]
 
-        cur.executemany(
-            "INSERT INTO %s (timestamp, close_USD) "
-            "VALUES (?, ?);" % self.db_table,
-            to_db)
+        cur.executemany("INSERT INTO %s (timestamp, close_USD) VALUES (?, ?);" % self.db_table, to_db)
         con.commit()
         return con
 
     def compute_avg_weekly_price_to_csv(self):
-        raise NotImplementedError
+        """
+        Groups timestamps by week, computes mean close price on each group and store the data frame into csv file.
+        """
+        c = self.connection.cursor()
+        self.print_datetime_output('Group time series by week and compute mean price')
+        query = "SELECT min((strftime('%Y%m%d', timestamp)/7*7 - 19000101) + 19000106) AS start_day, timestamp, " \
+                "avg(close_USD) FROM " + self.db_table + " GROUP BY (strftime('%Y%m%d', timestamp) - 19000106)/7"
+        c.execute(query)
+        avg_close_by_week_df = pd.DataFrame({x[0]: x[1:] for x in c.fetchall()})
+        self.print_datetime_output('Store data frame to file \'%s\'' % self.avg_price_file_name)
+        avg_close_by_week_df.to_csv('%s/%s' % (self.dir_path, self.avg_price_file_name))
 
     def get_week_of_max_relative_span(self):
-        raise NotImplementedError
+        """
+        Compute what is the week that had the greatest relative span on closing prices (difference between the maximum 
+        and minimum closing price, divided by the minimum closing price), and prints it on a screen.
+    
+        Mathematically: relative_span = (max(price) min(price)) / min(price)
+    
+        :return: date of a week with the maximum relative span on closing prices
+        """
+        c = self.connection.cursor()
+        self.print_datetime_output('Group time series by week and compute mean price')
+        query = "SELECT min((strftime('%Y%m%d', timestamp)/7*7 - 19000101) + 19000106) AS start_day, timestamp, " \
+                "min(close_USD), max(close_USD) FROM " + self.db_table + " GROUP BY (strftime('%Y%m%d', timestamp) - " \
+                                                                         "19000106)/7"
+        c.execute(query)
+        min_max_close_week_df = pd.DataFrame(c.fetchall())
+        min_max_close_week_df.columns = ['date', 'date_2', 'min', 'max']
+        min_max_close_week_df['min'] = min_max_close_week_df['min'].astype(float)
+        min_max_close_week_df['max'] = min_max_close_week_df['max'].astype(float)
+
+        min_max_close_week_df['rel_span'] = ((min_max_close_week_df['max'] - min_max_close_week_df['min']) /
+                                             min_max_close_week_df['min'])
+        max_rel_span = min_max_close_week_df['rel_span'].max()
+        week_max_rel_span = min_max_close_week_df[min_max_close_week_df['rel_span'] == max_rel_span]['date_2'].values[0]
+        self.print_datetime_output('The week with max relative span is: %s' % week_max_rel_span)
+        return week_max_rel_span
--- data/sqlite/test/test_sqlite_computations.py
+++ data/sqlite/test/test_sqlite_computations.py
@@ -1,16 +1,31 @@
-import sys
 import unittest
 
-# sys.path.append("..")
-from sqlite import sqlite_computations  # noqa
+import datetime
+import pandas as pd
+
+from sqlite import sqlite_computations
 
 
 class SqliteComputationsTestCase(unittest.TestCase):
     def setUp(self):
-        self.sqlite = sqlite_computations.Sqlite()
+        self.sqlite = sqlite_computations.Sqlite(True)
 
     def test_create_sqlite_table_created(self):
-        connection = self.sqlite.create_sqlite_table(True)
+        connection = self.sqlite.create_sqlite_table()
         cur = connection.cursor()
         cur.execute("select count(*) from sqlite_master where type='table' and name='%s'" % self.sqlite.db_table)
         self.assertEqual(cur.fetchall()[0][0], 1)
+
+    def test_compute_avg_weekly_price_df_returns_df(self):
+        self.sqlite.compute_avg_weekly_price_to_csv()
+        self.assertIsInstance(pd.read_csv('%s/%s' % (self.sqlite.dir_path, self.sqlite.avg_price_file_name)),
+                              pd.DataFrame)
+
+    def test_compute_avg_weekly_price_df_correct_calculations(self):
+        self.sqlite.compute_avg_weekly_price_to_csv()
+        avg_df = pd.read_csv('%s/%s' % (self.sqlite.dir_path, self.sqlite.avg_price_file_name))
+        self.assertEqual(avg_df.get('20140405')[1], '430.9361690214286')
+
+    def test_get_week_of_max_relative_span_in_memory_file(self):
+        week_max_rel_span = self.sqlite.get_week_of_max_relative_span()
+        self.assertEqual(datetime.datetime.strptime(week_max_rel_span, '%Y-%M-%d').date(), datetime.date(2015, 1, 9))
